cfffe41ed5e31101951c05629a7d8c385e705de9
kingmoc <joe.c.a.garcia@gmail.com>
2019-08-23 11:59:58 -0500

    Jokes Route Testing Complete

--- api/server.spec.js
+++ api/server.spec.js
@@ -62,9 +62,23 @@ describe('server', () => {
     })
 
     describe('GET /JOKES', () => {
+        it('Should Return 200 Status', () => {
+            return request(server).get('/api/jokes')
+                .set('authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjozLCJ1c2VybmFtZSI6ImthdGUiLCJpYXQiOjE1NjY1NzUyNDEsImV4cCI6MTU2NjY2MTY0MX0.7gUmJYKQyiazPxytLub2BzXK6F_KvurMOgNextLbnKQ')
+                .then(res => {
+                    expect(res.status).toBe(200)
+                })
+        })
+
+        it('should return an array', () => {
+            return request(server).get('/api/jokes')
+                .set('authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjozLCJ1c2VybmFtZSI6ImthdGUiLCJpYXQiOjE1NjY1NzUyNDEsImV4cCI6MTU2NjY2MTY0MX0.7gUmJYKQyiazPxytLub2BzXK6F_KvurMOgNextLbnKQ')
+                .then(res => {
+                expect(Array.isArray(res.body)).toBe(true);
+                });
+          });
 
 
-        
     })
 
 
